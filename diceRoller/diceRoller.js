c5f5125a754060a774948c7e89c709f152d7faf8
iam-baivab <baivabbandel@gmail.com>
2024-06-03 13:56:44 +0530

    Updated Dice(Manual)

--- diceRoller/diceRoller.js
+++ diceRoller/diceRoller.js
@@ -1,70 +1,16 @@
-window.onload = function(){
-    showErrorToast('Internal Server Error 500');
-}
-
-const showErrorToast = (message) => {
-    const toastContent = document.createElement('div');
-    toastContent.classList.add('toast-content');
-
-    const icon = document.createElement('i');
-    icon.classList.add('fas', 'fa-exclamation-circle', 'toast-icon');
-    icon.style.paddingLeft = '10px';
-    toastContent.appendChild(icon);
-
-    const messageElement = document.createElement('span');
-    messageElement.textContent = message;
-    toastContent.appendChild(messageElement);
-
-    const toast = Toastify({
-        node: toastContent,
-        duration: 3000,
-        gravity: 'top',
-        position: 'center',
-        backgroundColor: 'red',
-        progressBar: true,
-        style: {
-            padding: '20px 2px',
-            borderRadius: '8px',
-        }
-    });
-
-    const setToastWidth = () => {
-        const messageWidth = message.length * 10;
-        toast.options.style.maxWidth = `${messageWidth}px`;
-
-        if (window.innerWidth <= 768) {
-            toast.options.style.margin = '0 15px';
-        }
-    };
-
-    setToastWidth();
-
-    window.addEventListener('resize', setToastWidth);
-
-    toast.showToast();
-};
-
 document.addEventListener("DOMContentLoaded", () => {
     const dice = document.getElementById("dice");
     const rollBtn = document.getElementById("rollBtn");
 
-    // Function to fetch random number from API
-    function fetchRandomNumber() {
-        fetch('https://www.randomnumberapi.com/api/v1.0/random?min=1&max=7&count=1')
-            .then(response => response.json())
-            .then(data => {
-                const randomNumber = data[0]; // Extract the random number from the API response
-                updateDiceNumber(randomNumber);
-            })
-            .catch(error => console.error('Error fetching random number:', error));
-    }
+    rollBtn.addEventListener("click", () => {
+        dice.style.animation = "none";
+        void dice.offsetWidth;
+        dice.style.animation = "rollAnimation 1s ease-out forwards";
 
-    // Call fetchRandomNumber when the page loads
-    fetchRandomNumber();
-
-    // Call fetchRandomNumber when rollBtn is clicked
-    rollBtn.addEventListener("click", fetchRandomNumber => {
-        showErrorToast('Internal Server Error 500');
+        setTimeout(() => {
+            const randomNumber = Math.floor(Math.random() * 6) + 1;
+            updateDiceNumber(randomNumber);
+        }, 1000);
     });
 
     function updateDiceNumber(number) {
